5c058ed102a022c5d2699fd3cb2bda4b118e6f77
unknown <Tore@Blacksmith.(none)>
2012-04-03 08:25:30 +0200

    Intial commit fix, all files added again

---
+++ OnTopReplica/WindowsFormsApplication1/Platforms/WindowsXp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OnTopReplica.Platforms {
+    class WindowsXp : PlatformSupport {
+        
+        public override bool CheckCompatibility() {
+            MessageBox.Show("No DWM on this platform", "Error no DWM", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return false;
+        }
+
+    }
+}
