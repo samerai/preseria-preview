c44907a6925121f68ccdfb7176da35ba2a5c8a53
unknown <Tore@Blacksmith.(none)>
2012-04-03 08:39:49 +0200

    adding ontopreplica folder again hoping to fix conflict

---
+++ OnTopReplica/WindowsFormsApplication1/Platforms/Other.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OnTopReplica.Platforms {
+    class Other : PlatformSupport {
+        
+        public override bool CheckCompatibility() {
+            MessageBox.Show("Windows DWM/Aero not supported", "DWM not supported",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return false;
+        }
+
+    }
+}
